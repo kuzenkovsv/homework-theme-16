e27c3ed98341361eeb3e2cc707ea2fa36edea2d9
kuzenkovsv <kuzenkovsv@gmail.com>
2022-01-20 02:59:03 +0800

    initial

---
+++ MyMessages.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_theme_16
+{
+    public class MyMessages
+    {
+        public DateTime MessageTime { get; set; }
+        public string MessageText { get; set; }
+
+        public MyMessages(DateTime DateTime, string MessageText)
+        {
+            this.MessageTime = DateTime;
+            this.MessageText = MessageText;
+        }
+    }
+}
---
+++ ViewModel.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace homework_theme_16
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+
+        /// <summary>
+        /// Коллекция вычислений
+        /// </summary>
+        private ObservableCollection<MyMessages> calculationsdb = new ObservableCollection<MyMessages>();
+        public ObservableCollection<MyMessages> Calculationsdb
+        {
+            get
+            {
+                return calculationsdb;
+            }
+            set
+            {
+                calculationsdb = value;
+                OnPropertyChanged(nameof(calculationsdb));
+            }
+        }
+
+        /// <summary>
+        /// Информация о времени работы приложения
+        /// </summary>
+        private ObservableCollection<MyMessages> timeInformationdb = new ObservableCollection<MyMessages>();
+        public ObservableCollection<MyMessages> TimeInformationdb
+        {
+            get
+            {
+                return timeInformationdb;
+            }
+            set
+            {
+                timeInformationdb = value;
+                OnPropertyChanged(nameof(timeInformationdb));
+            }
+        }
+
+
+        /// <summary>
+        /// Вычисление
+        /// </summary>
+        private RelayCommand calculate;
+        public RelayCommand Calculate
+        {
+            get
+            {
+                return calculate ??
+                  (calculate = new RelayCommand(obj =>
+                  {
+                      MainWindow calc = obj as MainWindow;
+
+                      if (calc.OperationSelection.Text != null)
+                      {
+                          int res;
+                          switch (calc.OperationSelection.Text)
+                          {
+                              case "Сложение":
+                                  res = Convert.ToInt32(calc.Numb1.Text) + Convert.ToInt32(calc.Numb2.Text);
+                                  calc.Result.Text = Convert.ToString(res);
+                                  Calculationsdb.Add(new MyMessages(DateTime.Now, $"Сумма чисел {calc.Numb1.Text} и " +
+                                      $"{calc.Numb2.Text} равна {calc.Result.Text}"));
+                                  break;
+                              case "Вычитание":
+                                  res = Convert.ToInt32(calc.Numb1.Text) - Convert.ToInt32(calc.Numb2.Text);
+                                  calc.Result.Text = Convert.ToString(res);
+                                  Calculationsdb.Add(new MyMessages(DateTime.Now, $"{calc.Numb1.Text} вычесть " +
+                                      $"{calc.Numb2.Text} равно {calc.Result.Text}"));
+                                  break;
+
+                              default:
+                                  break;
+                          }
+                      }
+
+                  }));
+            }
+        }
+
+        void TimeInfo(ObservableCollection<MyMessages> TimeInformationdb)
+        {
+            try
+            {
+                for (int i = 0; i <= 2; i++)
+                {
+                    TimeInformationdb.Add(new MyMessages(DateTime.Now, $"С момента запуска прошло {i * 7} сек."));
+                    Thread.Sleep(7000);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"{e.Message}", "Ошибка!", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            
+        }
+        
+
+        async void TimeInfoAsync(ObservableCollection<MyMessages> TimeInformationdb)
+        {
+            await Task.Run(() => TimeInfo(TimeInformationdb));
+        }
+
+
+        public ViewModel()
+        {
+            TimeInfoAsync(TimeInformationdb);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
